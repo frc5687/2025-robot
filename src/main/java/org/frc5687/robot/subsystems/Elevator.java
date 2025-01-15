333f6eb260835924886cdf9ccc5d920e69bd18c0
Penguinsssssss <144957577+Penguinsssssss@users.noreply.github.com>
2025-01-15 15:39:47 -0500

    Test upload

---
+++ src/main/java/org/frc5687/robot/subsystems/Elevator.java
@@ -0,0 +1,24 @@
+
+
+package org.frc5687.robot.subsystems;
+
+import org.frc5687.robot.RobotContainer;
+
+
+
+// public class Elevator extends OutliersSubsystem() {
+//     public Elevator(RobotContainer container) {
+        
+//     }
+// }
+
+
+/* 
+ * class init
+ * create private motors (through motor id's and configs in constants.java)
+ * 
+ * create getters/setters (public function that can set and get things (angles, speeds))
+ * 
+ * create commands in classes/new files (require the subsytems that you are using and call its methods)
+ * 
+*/
