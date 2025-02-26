1f6cbb33dd532ba86d6eac02b19f2b69a2e165a0
lolmuffen <rafelilienthal@gmail.com>
2025-02-26 15:25:49 -0500

    Lightoutputs now extend base outputs

--- src/main/java/org/frc5687/robot/subsystems/lights/LightOutputs.java
+++ src/main/java/org/frc5687/robot/subsystems/lights/LightOutputs.java
@@ -1,11 +1,10 @@
 package org.frc5687.robot.subsystems.lights;
 
 import edu.wpi.first.epilogue.Logged;
+import org.frc5687.robot.util.BaseOutputs;
 
 @Logged
-public class LightOutputs {
+public class LightOutputs extends BaseOutputs {
     @Logged(name = "Light Desired State", importance = Logged.Importance.CRITICAL)
     public LightState desiredState = LightState.PLACEHOLDER;
 }
-
-
