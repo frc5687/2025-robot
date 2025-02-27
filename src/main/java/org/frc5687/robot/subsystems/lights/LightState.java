85405b4d69ebf67fcefa8dcb2aaaa4022cf5cef0
lolmuffen <rafelilienthal@gmail.com>
2025-02-27 17:38:36 -0500

    Added possible light state configs

--- src/main/java/org/frc5687/robot/subsystems/lights/LightState.java
+++ src/main/java/org/frc5687/robot/subsystems/lights/LightState.java
@@ -1,16 +1,36 @@
 package org.frc5687.robot.subsystems.lights;
 
 import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation.TwinklePercent;
 
+
 public enum LightState {
-    TWINKLE_DEFAULT(255, 255, 255);
+    TWINKLE_DEFAULT(0, 255, 255, 255),
+    FIRE_DEFAULT(1, 255, 255, 255),
+    SINGLE_FADE_DEFAULT(2, 255, 255, 255);
+    
 
     private final Animation _animation;
-
-    LightState(int r, int g, int b) {
-        _animation = new TwinkleAnimation(r, g, b, 0, 1.0, 100, TwinklePercent.Percent42);
+    LightState(int id, int r, int g, int b) {
+        //TODO find better settings for the animation
+        switch (id) {
+            case 0:
+                _animation = new TwinkleAnimation(r, g, b, 0, 1.0, 100, TwinklePercent.Percent42);
+                break;
+            case 1:
+                _animation = new FireAnimation(r, g, b, 0.5, 1.0, false, 0);
+                break;
+            case 2:
+                _animation = new SingleFadeAnimation(r, g, b, 0, 1.0, 100, 0);
+                break;
+            default:
+                _animation = new TwinkleAnimation(0, 0, 0, 0, 1.0, 100, TwinklePercent.Percent6);
+                break;
+        }
+        
     }
 
     public Animation getAnimation() {
