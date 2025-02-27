e3f1d337808d7b23bc7da31750038107a080149c
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-02-27 15:36:03 -0500

    change barge dropoff to not be locked

--- src/main/java/org/frc5687/robot/subsystems/algaearm/AlgaeState.java
+++ src/main/java/org/frc5687/robot/subsystems/algaearm/AlgaeState.java
@@ -1,11 +1,15 @@
 package org.frc5687.robot.subsystems.algaearm;
 
+import org.frc5687.robot.Constants;
+
+import edu.wpi.first.math.util.Units;
+
 public enum AlgaeState {
     IDLE(1.818), // ratchet is locked in this state :)
     GROUND_PICKUP(3.6),
     REEF_PICKUP(3.465),
     PROCESSOR_DROPOFF(2.75762),
-    BARGE_DROPOFF(Math.PI / 2),
+    BARGE_DROPOFF(Constants.AlgaeArm.TOP_EJECT_SAFE_ANGLE - Units.degreesToRadians(5)),
     CORAL_DODGE(2.566);
 
     private final double _armAngle;
