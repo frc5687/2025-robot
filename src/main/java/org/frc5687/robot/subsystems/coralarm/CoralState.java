3a3d7aed026c719b465667f487c04377fad7a57e
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-02-28 17:48:17 -0500

    tweak L4 coral place angle

--- src/main/java/org/frc5687/robot/subsystems/coralarm/CoralState.java
+++ src/main/java/org/frc5687/robot/subsystems/coralarm/CoralState.java
@@ -7,7 +7,7 @@ public enum CoralState {
     IDLE(Units.degreesToRadians(30 + 90)),
     RECEIVE_FROM_FUNNEL(Constants.CoralArm.MAX_ANGLE),
     PLACING(Units.degreesToRadians(30 + 90)), // 27.5 deg
-    PLACING_L4(Units.degreesToRadians(30 + 90 + 15)); // 27.5 deg
+    PLACING_L4(Units.degreesToRadians(30 + 90 + 10)); // 27.5 deg
 
     private final double _armAngle;
 
