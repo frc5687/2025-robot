cd99c974553060c89d722c455739575ae38dec3e
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-03-01 10:44:38 -0500

    reefc algae setopints

--- src/main/java/org/frc5687/robot/subsystems/algaearm/AlgaeState.java
+++ src/main/java/org/frc5687/robot/subsystems/algaearm/AlgaeState.java
@@ -3,7 +3,7 @@ package org.frc5687.robot.subsystems.algaearm;
 public enum AlgaeState {
     IDLE(2.312), // wheel is not touching in this state
     GROUND_PICKUP(3.7),
-    REEF_PICKUP(3.7),
+    REEF_PICKUP(3.5),
     PROCESSOR_DROPOFF(3.4),
     BARGE_DROPOFF(1.575),
     CORAL_DODGE(2.566);
