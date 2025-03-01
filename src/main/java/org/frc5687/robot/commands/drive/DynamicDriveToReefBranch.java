25d15b33076b2a71b94e987f6d795fe277c02564
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-03-01 18:40:51 -0500

    un-hard code alliance check for DynamicDriveToReef

--- src/main/java/org/frc5687/robot/commands/drive/DynamicDriveToReefBranch.java
+++ src/main/java/org/frc5687/robot/commands/drive/DynamicDriveToReefBranch.java
@@ -3,6 +3,7 @@ package org.frc5687.robot.commands.drive;
 import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import org.frc5687.robot.subsystems.drive.DriveSubsystem;
 import org.frc5687.robot.util.ReefAlignmentHelpers;
 import org.frc5687.robot.util.ReefAlignmentHelpers.ReefSide;
@@ -15,8 +16,7 @@ public class DynamicDriveToReefBranch extends DriveToPoseSmooth {
                 () -> {
                     Pose2d rawPose = drive.getPose();
                     var alliance = DriverStation.getAlliance();
-                    // if (alliance.isPresent() && alliance.get() == Alliance.Red) {
-                    if (false) {
+                    if (alliance.isPresent() && alliance.get() == Alliance.Red) {
                         Pose2d mirroredPose = FlippingUtil.flipFieldPose(rawPose);
                         int currentFace = ReefAlignmentHelpers.calculateBestFace(mirroredPose);
                         Pose2d mirroredTargetPose = ReefAlignmentHelpers.calculateTargetPose(currentFace, side);
