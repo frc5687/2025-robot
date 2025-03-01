41024c49e86fb6b9a76714f47bd26f17be1031af
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-03-01 10:45:05 -0500

    fix autoeject

--- src/main/java/org/frc5687/robot/commands/auto/AutoActions.java
+++ src/main/java/org/frc5687/robot/commands/auto/AutoActions.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import org.frc5687.robot.Constants;
 import org.frc5687.robot.RobotContainer;
 
 public class AutoActions {
@@ -12,7 +13,9 @@ public class AutoActions {
         return new SequentialCommandGroup(
                 new InstantCommand(
                         () -> {
-                            container.getCoral().setWheelMotorDutyCycle(1);
+                            container
+                                    .getCoral()
+                                    .setWheelMotorDutyCycle(Constants.CoralArm.WHEEL_EJECT_CORAL_DUTY_CYCLE);
                         }),
                 new WaitCommand(0.3),
                 new InstantCommand(
