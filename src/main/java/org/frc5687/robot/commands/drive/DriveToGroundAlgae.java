f57f5cda1b698acf935b8044667a334aa8ac1e65
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-03-28 08:59:45 -0400

    tested ground algae in sim

--- src/main/java/org/frc5687/robot/commands/drive/DriveToGroundAlgae.java
+++ src/main/java/org/frc5687/robot/commands/drive/DriveToGroundAlgae.java
@@ -3,10 +3,13 @@ package org.frc5687.robot.commands.drive;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import java.util.Optional;
 import org.frc5687.robot.RobotStateManager;
 import org.frc5687.robot.RobotStateManager.RobotCoordinate;
 import org.frc5687.robot.subsystems.drive.DriveSubsystem;
 import org.frc5687.robot.subsystems.vision.VisionSubsystem;
+import org.frc5687.robot.util.FieldConstants;
 import org.frc5687.robot.util.TunableDouble;
 import org.frc5687.robot.util.vision.AlgaeTracker;
 
@@ -15,9 +18,9 @@ public class DriveToGroundAlgae extends DriveToPoseSmooth {
     private final VisionSubsystem _vision;
 
     private static final TunableDouble xOffset =
-            new TunableDouble("DriveToGroundAlgae", "xOffset", 0);
+            new TunableDouble("DriveToGroundAlgae", "xOffset", 0.6);
     private static final TunableDouble yOffset =
-            new TunableDouble("DriveToGroundAlgae", "yOffset", 0);
+            new TunableDouble("DriveToGroundAlgae", "yOffset", Units.inchesToMeters(-6.75));
 
     public DriveToGroundAlgae(DriveSubsystem drive, VisionSubsystem vision) {
         super(
@@ -26,7 +29,9 @@ public class DriveToGroundAlgae extends DriveToPoseSmooth {
                     var robotPose =
                             RobotStateManager.getInstance().getPose(RobotCoordinate.ROBOT_BASE_SWERVE).toPose2d();
                     var detection = AlgaeTracker.getInstance().getClosestAlgae(robotPose.getTranslation());
-                    // var detection = Optional.of(Translation2d.kZero); // FIXME remove
+                    // var detection =
+                    //         Optional.of(
+                    //                 FieldConstants.StagingPositions.rightIceCream.getTranslation()); // FIXME remove
                     if (detection.isEmpty()) {
                         return robotPose;
                     }
@@ -40,7 +45,6 @@ public class DriveToGroundAlgae extends DriveToPoseSmooth {
 
                     return new Pose2d(robotTargetTranslation, theta);
                 });
-        _rotationController.setPID(8, 0, 0);
         _drive = drive;
         _vision = vision;
         addRequirements(_drive);
