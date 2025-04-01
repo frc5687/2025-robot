3dcd1ce142376a39b6151c2ecdd5c174262e91d7
de.slobodzian <de.slobodzian@gmail.com>
2025-04-01 00:32:05 -0400

    auto actions for algae

--- src/main/java/org/frc5687/robot/commands/drive/DriveToProcessorLineup.java
+++ src/main/java/org/frc5687/robot/commands/drive/DriveToProcessorLineup.java
@@ -4,19 +4,22 @@ import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import org.frc5687.robot.subsystems.drive.DriveSubsystem;
+import org.frc5687.robot.subsystems.superstructure.SuperstructureManager;
 import org.frc5687.robot.util.FieldConstants;
 
-public class DriveToProcessorLineup extends DriveToPoseSmooth {
-    public DriveToProcessorLineup(DriveSubsystem drive) {
+public class DriveToProcessorLineup extends DriveWithNormalVectorAlignment {
+    public DriveToProcessorLineup(DriveSubsystem drive, SuperstructureManager manager) {
         super(
                 drive,
+                manager,
                 () -> {
                     var alliance = DriverStation.getAlliance();
                     if (alliance.isPresent() && alliance.get() == Alliance.Red) {
-                        return FlippingUtil.flipFieldPose(FieldConstants.Processor.processorLineUp);
+                        return FlippingUtil.flipFieldPose(FieldConstants.Processor.processorLineUpClose);
                     } else {
-                        return FieldConstants.Processor.processorLineUp;
+                        return FieldConstants.Processor.processorLineUpClose;
                     }
-                });
+                },
+                true);
     }
 }
