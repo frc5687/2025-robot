d3f91dfdd9f9b0a7019fd606623ca0d7eb98cdca
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2025-04-03 20:06:38 -0400

    add another +2deg to auto coral setpoint

--- src/main/java/org/frc5687/robot/subsystems/coralarm/CoralState.java
+++ src/main/java/org/frc5687/robot/subsystems/coralarm/CoralState.java
@@ -10,7 +10,7 @@ public enum CoralState {
     PLACING_L1(4.4),
     PLACING(Units.degreesToRadians(30 + 90)), // 27.5 deg
     PLACING_L4(Units.degreesToRadians(30 + 90 + 10)), // 27.5 deg
-    PLACING_L4_AUTO(Units.degreesToRadians(30 + 90 + 10 + 3)); // a bit lower
+    PLACING_L4_AUTO(Units.degreesToRadians(30 + 90 + 10 + 5)); // a bit lower
 
     private final double _armAngle;
 
